18275f069b5056e9aa659e0311771b057fb8ce6e
KrzysiekPietrzak <krzysztof.pietrzak17@gmail.com>
2023-01-15 19:17:20 +0100

    first one!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+const [name,setName] = useState("");
+
+const addName = () =>{
+  console.log("started");
+  Axios.post("http://localhost:3001/create",{
+    name: name,
+  }).then(()=>{
+    console.log("Succ");
+  }
+  );
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<label>Name:</label>
+<input type="text" 
+onChange={(event)=>{
+  setName(event.target.value);
+  
+}}/>
+<hr />
+<button onClick={addName}> Add</button>
     </div>
   );
 }
