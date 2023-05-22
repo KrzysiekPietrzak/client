6551d93a1c773ad81a50a274d48a8eab32c11eb9
KrzysiekPietrzak <Krzysztof.pietrzak17@gmail.com>
2023-05-22 19:21:06 +0200

    commit 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,7 +13,7 @@ const Login =()=> {
   async function loginUser(event){
     event.preventDefault()
 
-    const response = await fetch('http://localhost:1337/api/login',{
+    const response = await fetch('https://serv4.onrender.com/api/login',{
       method: 'POST',
       headers:{
         'Content-Type':'application/json'
