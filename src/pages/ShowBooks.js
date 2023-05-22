8251433ef770387040c136d091cb1dd9e1821b53
KrzysiekPietrzak <Krzysztof.pietrzak17@gmail.com>
2023-05-22 21:38:54 +0200

    9

--- src/pages/ShowBooks.js
+++ src/pages/ShowBooks.js
@@ -1,15 +1,13 @@
-import {useParams} from 'react-router-dom'
 import React, { useEffect, useState } from "react"
 import BookBio from "../component/BookBio"
 
 
 function App(){
-      const params = useParams()
       
   const [data, setData] = useState([])
 
         const fetchData = async () => {
-    const response = await fetch("'https://serv4.onrender.com/books")
+    const response = await fetch("https://serv4.onrender.com/books")
     console.log(await response.json())
     const dataBook = await response.json()
         setData(dataBook)
